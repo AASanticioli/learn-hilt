e6fa09be8d400f7324e794c24c551235c3824be8
Santicioli <67654539+AASanticioli@users.noreply.github.com>
2023-04-15 09:41:32 -0300

    Basic implementation finished

--- app/src/main/java/com/example/android/hilt/ui/LogsFragment.kt
+++ app/src/main/java/com/example/android/hilt/ui/LogsFragment.kt
@@ -17,7 +17,6 @@
 package com.example.android.hilt.ui
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -25,26 +24,31 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
-import com.example.android.hilt.LogApplication
 import com.example.android.hilt.R
 import com.example.android.hilt.data.Log
 import com.example.android.hilt.data.LoggerLocalDataSource
 import com.example.android.hilt.util.DateFormatter
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 /**
  * Fragment that displays the database logs.
  */
+@AndroidEntryPoint
 class LogsFragment : Fragment() {
 
-    private lateinit var logger: LoggerLocalDataSource
-    private lateinit var dateFormatter: DateFormatter
+    @Inject
+    lateinit var logger: LoggerLocalDataSource
+
+    @Inject
+    lateinit var dateFormatter: DateFormatter
 
     private lateinit var recyclerView: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
         return inflater.inflate(R.layout.fragment_logs, container, false)
     }
@@ -55,27 +59,13 @@ class LogsFragment : Fragment() {
         }
     }
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-
-        populateFields(context)
-    }
-
-    private fun populateFields(context: Context) {
-        logger = (context.applicationContext as LogApplication).serviceLocator.loggerLocalDataSource
-        dateFormatter =
-            (context.applicationContext as LogApplication).serviceLocator.provideDateFormatter()
-    }
-
     override fun onResume() {
         super.onResume()
 
         logger.getAllLogs { logs ->
-            recyclerView.adapter =
-                LogsViewAdapter(
-                    logs,
-                    dateFormatter
-                )
+            recyclerView.adapter = LogsViewAdapter(
+                logs, dateFormatter
+            )
         }
     }
 }
@@ -85,15 +75,14 @@ class LogsFragment : Fragment() {
  */
 private class LogsViewAdapter(
     private val logsDataSet: List<Log>,
-    private val daterFormatter: DateFormatter
+    private val daterFormatter: DateFormatter,
 ) : RecyclerView.Adapter<LogsViewAdapter.LogsViewHolder>() {
 
     class LogsViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LogsViewHolder {
         return LogsViewHolder(
-            LayoutInflater.from(parent.context)
-                .inflate(R.layout.text_row_item, parent, false) as TextView
+            LayoutInflater.from(parent.context).inflate(R.layout.text_row_item, parent, false) as TextView
         )
     }
 
--- app/src/main/java/com/example/android/hilt/ui/MainActivity.kt
+++ app/src/main/java/com/example/android/hilt/ui/MainActivity.kt
@@ -17,21 +17,25 @@
 package com.example.android.hilt.ui
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
-import com.example.android.hilt.LogApplication
 import com.example.android.hilt.R
 import com.example.android.hilt.navigator.AppNavigator
 import com.example.android.hilt.navigator.Screens
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 /**
  * Main activity of the application.
  *
  * Container for the Buttons & Logs fragments. This activity simply tracks clicks on buttons.
  */
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var navigator: AppNavigator
+    @Inject
+    lateinit var navigator: AppNavigator
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,8 +46,6 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun setupNavigation(savedInstanceState: Bundle?) {
-        navigator = (applicationContext as LogApplication).serviceLocator.provideNavigator(this)
-
         if (savedInstanceState == null) {
             navigator.navigateTo(Screens.BUTTONS)
         }
@@ -55,6 +57,7 @@ class MainActivity : AppCompatActivity() {
             /* onBackPressedCallback = */
             object : OnBackPressedCallback(/* enabled = */ true) {
                 override fun handleOnBackPressed() {
+                    Toast.makeText(this@MainActivity, "back pressed", Toast.LENGTH_SHORT).show()
                     if (supportFragmentManager.backStackEntryCount == 0) {
                         finish()
                     }
